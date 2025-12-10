1b2b29ea760f27d70a89571f88848c6c6373a161
adira29 <iamaditirani29@gmail.com>
2025-12-10 07:49:26 +0530

    add all files till now

--- assignment4.py
+++ assignment4.py
@@ -5,3 +5,20 @@ number = input("eneter the teo digit number ")
 first_digit =number[0]
 second_digit = number[1]
 print(int(first_digit) + int(second_digit))
+
+a= 5
+print(a)
+
+a,b,c = 9,6,9
+a+=2
+print(a,b,c)
+
+a,b= 4,3
+c = a+b
+a+=2
+c+=a
+print(c)
+
+a,b = 4,3
+
+print(a<3 and b ==3)
---
+++ list.py
@@ -0,0 +1,17 @@
+# numbers = [10,0,-1,7]
+# names = ['aditi','krishna','Ram']
+# print(numbers)
+# print(names)
+# mix_list= [1,'aditi',True,10.10]
+# print(mix_list)
+# print(numbers[-2])
+
+numbers = [10,0,-1,7,8,10,-67]
+# numbers.sort()
+numbers.append(45) #at the end
+numbers.insert(2,55) #at the index specific position
+# print(max(numbers))
+numbers.extend([56,89,23]) #at the end
+numbers.remove(0)
+numbers.pop()
+print(numbers)
